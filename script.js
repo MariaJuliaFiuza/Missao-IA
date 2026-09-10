1ef40c2f6eefd43d9f87fda11a69ef41a12d77c5
Maria Julia Fiuza <m.gula@escola.pr.gov.br>
2026-09-10 12:04:42 -0300

    ...

--- script.js
+++ script.js
@@ -6,30 +6,30 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "O cyberbullying é a violência virtual que ocorre geralmente com as pessoas tímidas e indefesas, ou simplesmente por não caírem na simpatia das mesmas. Como podemos evitá-lo",
+        enunciado: " Qual é um dos principais desafios da saúde pública?",
         alternativas: [
             {
-                texto: "Utilizando palestras para prevenir o cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa reflexiva e busca aprendizado e conhecimento."
+                texto: "Garantir atendimento de qualidade para toda a população.",
+                afirmacao: "Você se preocupada com a igualdade, a qualidade do atendimento e a redução das diferenças no acesso à saúde"
             },
             {
-                texto:  "Prestando atenção aos sinais de cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos."
+                texto:  "Reduzir os gastos públicos com saúde.",
+                afirmacao: "Você  acredita que controlar os custos e utilizar melhor os recursos públicos é uma prioridade."
             }    
            
         ]
     },
     {
        
-            enunciado: "As fontes renováveis de energia são aquelas formas de produção de energia em que suas fontes são capazes de manter-se disponíveis durante um longo prazo, contando com recursos que se regeneram ou que se mantêm ativos permanentemente. Com base nisso, temos a energia geotérmica, que consiste em utilizar o calor manifestado em áreas próximas à superfície. Qual a sua opinião sobre esse modelo de energia renovável?",
+            enunciado: "Quem deve ter acesso aos serviços de saúde pública?",
             alternativas: [
                 {
-                    texto: "A energia geotérmica precisa ser mais explorada visto que utiliza o calor interno da terra, pois não depende do clima e é inesgotável.",
-                    afirmacao: "Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores."
+                    texto: " Todas as pessoas, independentemente de sua condição financeira.",
+                    afirmacao: "Você defende a saúde como um direito de todos e valoriza a igualdade no acesso aos serviços."
                 },
                 {
-                    texto:  "Seria interessante explorar primeiramente fontes renováveis exotérmicas, em segundo plano utilizar a energia geotérmica apesar dos custos elevados de explorações e possibilidades de contaminação de rios. ",
-                    afirmacao: "Você é uma pessoa que dá ênfase em priorizar os recursos disponíveis, pensando no meio ambiente e sem descartar o viés econômico."
+                    texto:  "Principalmente as pessoas que não podem pagar por atendimento particular.",
+                    afirmacao: ""
                 }    
                
             ]
